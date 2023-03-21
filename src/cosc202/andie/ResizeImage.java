2e4d673df40fb916e9b1825a4418fc859d193497
Nick Garner <garni988@student.otago.ac.nz>
2023-03-21 13:57:25 +1300

    Got started on ResizeImage

--- src/cosc202/andie/ResizeImage.java
+++ src/cosc202/andie/ResizeImage.java
@@ -1,13 +1,25 @@
 package cosc202.andie;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class ResizeImage implements ImageOperation, java.io.Serializable {
 
-    @Override
+    private int resizePercent = 0;
+
+    public ResizeImage(){
+
+    }
+
+    public ResizeImage(int resizePercent){
+        this.resizePercent = resizePercent;
+    }
+
     public BufferedImage apply(BufferedImage input) {
-        
-        
+        //Calculate dimensions for resized image
+        int scaledWidth = (int) (input.getWidth() * resizePercent);
+        int scaledHeight = (int) (input.getHeight() * resizePercent);
+
         return input;
     }
     
