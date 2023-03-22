b2efc837c1d183ee192738b48d1061b4d6e07750
Luke <weblu938@student.otago.ac.nz>
2023-03-22 21:54:06 +1300

    Added additional test for ImagePanel

--- src/test/cosc202/andie/ImagePanelTest.java
+++ src/test/cosc202/andie/ImagePanelTest.java
@@ -1,5 +1,6 @@
 package test.cosc202.andie;
 
+import java.awt.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +16,7 @@ public class ImagePanelTest {
     void getZoomInitialValue(){
         ImagePanel testPanel = new ImagePanel();
         Assertions.assertEquals(100.0, testPanel.getZoom());
+        Assertions.assertEquals(-1, testPanel.getZoom());//this test should fail on purpose
     }
 
     //Tests zoom function
@@ -22,10 +24,18 @@ public class ImagePanelTest {
     void getZoomAfterSetZoom(){
         ImagePanel testPanel = new ImagePanel();
         testPanel.setZoom(0.0);
+        
         Assertions.assertFalse(testPanel.getZoom() == 100.0);
         Assertions.assertTrue(testPanel.getZoom() >= 50.0);
-
     }
 
+    //Tests that the default preffered size of an image is 450 x 450
+    @Test
+    void getDefaultPrefferedSize(){
+        ImagePanel testPanel = new ImagePanel();
+        Dimension dimension = new Dimension(450, 450);
+        Assertions.assertEquals(dimension.getWidth(), testPanel.getPreferredSize().getWidth());
+        Assertions.assertEquals(dimension.getHeight(), testPanel.getPreferredSize().getHeight());
+    } 
 
 }
