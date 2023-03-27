90af41ef1a0afb1d2cdcc38e4b390f73e9e1ed96
weblu938 <weblu938@student.otago.ac.nz>
2023-03-28 12:41:55 +1300

    Finished Tests for MedianFilter

--- src/test/cosc202/andie/MedianFilterTest.java
+++ src/test/cosc202/andie/MedianFilterTest.java
@@ -1,9 +1,11 @@
 package test.cosc202.andie;
+
 import java.awt.image.BufferedImage;
-import java.awt.image.*;
-import java.util.*;
+import javax.imageio.*;
+import java.io.*;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import cosc202.andie.MedianFilter;
@@ -11,6 +13,17 @@ import cosc202.andie.MedianFilter;
 public class MedianFilterTest {
 
     private final MedianFilter testFilter1 = new MedianFilter();
+    private final MedianFilter testFilter2 = new MedianFilter(3);
+    private static BufferedImage testingImage;
+
+    @BeforeAll
+    static void getImage(){
+        try{
+            testingImage = ImageIO.read(new File("/andie/clocktower.jpg"));
+        }catch (Exception e){
+            System.out.println("Failed to find image");
+        }
+    }
 
     //Dummy Test
     @Test
@@ -20,10 +33,9 @@ public class MedianFilterTest {
     //Tests Default radius is 1.
     @Test
     void getDefaultRadius(){
-        
         MedianFilter filter = new MedianFilter(1);
         Assertions.assertEquals(filter.getRadius(), testFilter1.getRadius());
-        Assertions.assertTrue(1 == testFilter1.getRadius());
+        Assertions.assertTrue(1 == filter.getRadius());
     }
 
     //Tests that if we call constructor passing a value it will change the radius.
@@ -33,12 +45,40 @@ public class MedianFilterTest {
         Assertions.assertTrue(9 == filter.getRadius());
     }
 
-    //Test that the correct image operation has been completed
+    //Test that the correct image operation has been completed using default radius
     @Test
-    void testImages(){
-        MedianFilter filter2 = new MedianFilter(2);
-        //filter2.apply(null);
+    void imageTest1(){
+        MedianFilter filter = new MedianFilter();
+        try{
+            BufferedImage manualImage = ImageIO.read(new File("/andie/clocktower.jpg"));
+            BufferedImage testImage1 = filter.apply(manualImage);
+            BufferedImage testImage2 = testFilter1.apply(testingImage);
+            Assertions.assertEquals(testImage1.getRGB(1, 1),testImage2.getRGB(1, 1));
+            Assertions.assertEquals(testImage1.getRGB(30, 150),testImage2.getRGB(30, 150));
+            Assertions.assertEquals(testImage1.getRGB(109, 800),testImage2.getRGB(109, 800));
+        }catch (Exception E){
+            System.out.println("Failed to find image");
+        }
     }
+
+    
+    //Test that the correct image operation has been completed using a larger radius, 3
+    @Test
+    void imageTest2(){
+        MedianFilter filter = new MedianFilter(3);
+        try{
+            BufferedImage manualImage = ImageIO.read(new File("/andie/clocktower.jpg"));
+            BufferedImage testImage1 = filter.apply(manualImage);
+            BufferedImage testImage2 = testFilter2.apply(testingImage);
+            Assertions.assertEquals(testImage1.getRGB(1, 1),testImage2.getRGB(1, 1));
+            Assertions.assertEquals(testImage1.getRGB(30, 150),testImage2.getRGB(30, 150));
+            Assertions.assertEquals(testImage1.getRGB(109, 800),testImage2.getRGB(109, 800));
+        }catch (Exception E){
+            System.out.println("Failed to find image");
+        }
+    }
+
+
 
 
 }
