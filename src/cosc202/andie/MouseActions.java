ea2e0a0a546ed90c973da97f5cec9fc1ef2ba6cd
Nick Garner <garni988@student.otago.ac.nz>
2023-05-22 14:14:52 +1200

    Added javadoc comments to cropper and mouseactions

--- src/cosc202/andie/MouseActions.java
+++ src/cosc202/andie/MouseActions.java
@@ -6,16 +6,32 @@ import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
 import javax.swing.* ;
-
+/**
+ * <p>
+ * Class for Mouse Actions
+ * </p>
+ * 
+ * <p>
+ * This class adds to the Mouse menu and calls to their respective classes
+ * This includes a crop function
+ * </p>
+ *  
+ * @author Samuel Goddard
+ * @version 1.0
+ */
 public class MouseActions implements KeyListener {
     
+    /** A list of actions for the Mouse menu. */
     protected ArrayList<Action> actions ; 
-
+    /**
+     * <p>
+     * Create a set of Mouse menu actions.
+     * </p>
+     */
     public MouseActions() {
        
         actions = new ArrayList<Action>();
         actions.add( new CropAction( Andie.getLanguage("crop") , null , Andie.getLanguage("crop_description") , Integer.valueOf( KeyEvent.VK_X ) ) ) ;
-        actions.add( new StopAction( "Stop" , null , "Stops Crop Image" , Integer.valueOf( 0 ) ) ) ;
 
     }
 
@@ -40,6 +56,12 @@ public class MouseActions implements KeyListener {
     @Override
     public void keyReleased(KeyEvent e) {}
 
+    /**
+     * <p>
+     * Creates a menu containing a list of all mouse actions
+     * </p>
+     * @return the mouse menu
+     */
     public JMenu createMenu() {
         
         JMenu mouseMenu = new JMenu( "Mouse" ) ;
@@ -53,39 +75,76 @@ public class MouseActions implements KeyListener {
         return mouseMenu;
 
     }
-
+    /**
+     * <p>
+     * Action to start the action of cropping an image
+     * </p>
+     * 
+     * @see Cropper
+     */
     public class CropAction extends ImageAction {
-
+        /**
+         * <p>
+         * Starts cropping action
+         * </p>
+         * 
+         * @param name The name of the action (ignored if null).
+         * @param icon An icon to use to represent the action (ignored if null).
+         * @param desc A brief description of the action  (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut  (ignored if null).
+         */
         CropAction( String name , ImageIcon icon , String desc , Integer mnemonic ) {
 
             super( name , icon , desc , mnemonic ) ;
 
 
         }
-
+        /**
+         * <p>
+         * Callback for when crop is selected
+         * </p>
+         * 
+         * @param e The event triggering this callback. 
+         */
         public void actionPerformed( ActionEvent e ) {
 
-            // if ( target.getImage().hasImage() == true ) {}
-
                 target.cropActive( true ) ; 
-
-        // }   else {
-
-
-        //     }
             
         }
 
     }
 
+    /**
+     * <p>
+     * Action to stop the action of cropping an image
+     * </p>
+     * 
+     * @see Cropper
+     */
     public class StopAction extends ImageAction {
 
+        /**
+         * <p>
+         * Stops cropping action
+         * </p>
+         * 
+         * @param name The name of the action (ignored if null).
+         * @param icon An icon to use to represent the action (ignored if null).
+         * @param desc A brief description of the action  (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut  (ignored if null).
+         */
         StopAction( String name , ImageIcon icon , String desc , Integer mnemonic ) {
 
             super( name , icon , desc , mnemonic ) ;
 
         }
-
+        /**
+         * <p>
+         * Callback for when stopping crop is confirmed
+         * </p>
+         * 
+         * @param e The event triggering this callback. 
+         */
         public void actionPerformed( ActionEvent e ) {
 
             target.cropActive( false ) ; 
